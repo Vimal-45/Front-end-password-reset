fdea5311dfc93b3048f018af7063d31edaa3a905
Vimal-45 <dvimal45@gmail.com>
2023-11-12 00:58:08 +0530

    add loding commend

--- src/Components/ResetPassword.jsx
+++ src/Components/ResetPassword.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom"
 const ResetPassword = () => {
     const navigate = useNavigate()
     const [email, setEmail] = useState('')
+    const [loading, setLoading] = useState(false);
 
     const myRef = useRef() 
 
@@ -18,15 +19,17 @@ const ResetPassword = () => {
     const handleSubmit = () => {
         if (!email) { alert('please enter the valid email') }
         else {
+            setLoading(true);
             axios.post('https://password-reset-5gb9.onrender.com/api/user/forgot-password', { email: email })
                 .then(res => {
 
                     localStorage.setItem('STRING', res.data.randomString);
                     localStorage.setItem('TOKEN', res.data.token);
                     if(!res.data.message){
+                        setLoading(false);
                         return (
                             <>
-                                <h1>Loading.....</h1>
+                                <h1>Sending Link.....</h1>
                             </>
                         )
                     }
